d48a8867f73845d3385b6d8d8169174d935cdf7b
RustyBamboo <dvolya@gmail.com>
2018-01-29 18:25:04 -0500

    Introduce basic framework and screen changing
    - Simple splash screen with animation
    - Splash screen my be skipped by pressing space
    - Screen can be skipped only once flag 'loading' is false
    - Basic main menu/starting scene

---
+++ main.gd
@@ -0,0 +1,42 @@
+extends Control
+
+export (PackedScene) var next_scene
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+# Loading Thread
+onready var loading_thread = Thread.new()
+var next_scene_instance = null
+
+func _ready():
+	print ("Started")
+	# Load Data...
+	loading_thread.start(self, "load_data")
+	
+	# Splash Screen ...
+	splash_screen()
+
+func splash_screen():
+	print("Loading splash screen...")
+	
+	# Create instance
+	next_scene_instance = next_scene.instance()
+	
+	# Add to scene
+	add_child(next_scene_instance)
+	
+func load_data(input):
+	# Load data...
+	print ("Loading data...")
+	for i in range(0, 990):
+		for j in range(0, 7):
+			pass
+	print ("Done loading data...")
+	next_scene_instance.is_loading = false
+	
+	
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
