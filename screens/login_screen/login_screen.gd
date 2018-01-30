ab7ee0f5463db9db0c396d31bc3c7d280e061620
RustyBamboo <dvolya@gmail.com>
2018-01-29 21:55:20 -0500

    Modify screen states to load login first

--- screens/login_screen/login_screen.gd
+++ screens/login_screen/login_screen.gd
@@ -1,29 +1,23 @@
 extends Control
 
 export (PackedScene) var next_scene
-var next_scene_instance = null
 
 onready var username_field = get_node("TextEdit")
 onready var password_field = get_node("TextEdit2")
+
 func _ready():
 	print ("loaded login!")
 
 # This has been connected from Button
-func _on_Button_pressed():
+func _on_Button_button_up():
 	print("Button Pressed!")
 	print(username_field.get_line(0))
 	print(password_field.get_line(0))
-	
+
 	# do authentication here
 	
-	# Splash Screen ...
-	splash_screen()
+	get_tree().change_scene_to(next_scene)
 
-func splash_screen():
-	print("Loading splash screen...")
-	
-	# Create instance
-	next_scene_instance = next_scene.instance()
-	get_parent().add_child(next_scene.instance())
-	next_scene_instance.is_loading = false
-	queue_free()
+
+
+
