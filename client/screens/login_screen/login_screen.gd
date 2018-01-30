e586d209caa0009a54ce3abe52c450cc95622cfe
RustyBamboo <dvolya@gmail.com>
2018-01-30 11:00:30 -0500

    Directory cleanup

--- client/screens/login_screen/login_screen.gd
+++ client/screens/login_screen/login_screen.gd
@@ -2,8 +2,9 @@ extends Control
 
 export (PackedScene) var next_scene
 
-onready var username_field = get_node("TextEdit")
-onready var password_field = get_node("TextEdit2")
+onready var username_field = get_node("Username_field")
+onready var password_field = get_node("Password_field")
+onready var address_field = get_node("Address_field")
 
 func _ready():
 	print ("loaded login!")
@@ -13,10 +14,12 @@ func _on_Button_button_up():
 	print("Button Pressed!")
 	print(username_field.get_line(0))
 	print(password_field.get_line(0))
+	print(address_field.get_line(0))
 
 	# do authentication here
 	
 	get_tree().change_scene_to(next_scene)
+	queue_free()
 
 
 
