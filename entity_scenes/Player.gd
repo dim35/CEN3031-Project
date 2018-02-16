6bb4ba3fcb247b77b79fd4bc5a90518459bf08cd
Aleksandr Hovhannisyan <aleksandrhovhannisyan@gmail.com>
2018-02-15 20:01:03 -0500

    Changed player animations

--- entity_scenes/Player.gd
+++ entity_scenes/Player.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends KinematicBody2D
 # The player's speed
 export (int) var SPEED
 # The player's velocity
@@ -7,29 +7,35 @@ var velocity = Vector2()
 
 
 func _ready():	
-	
 	pass
 
 
 
-func _process(delta):
+func _physics_process(delta):
+	
+	# Reset velocity to blank slate
 	velocity.x = 0
 	velocity.y = 0	
 	
+	# Player is moving to the right
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
 		
+	# Player is moving to the left
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
 	
+	# If player moved
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * SPEED
 		$Animations.play()
 	else:
 		$Animations.animation = "idle"
 	
+	# Determine walking animation direction
 	if velocity.x != 0:
 		$Animations.animation = "walking"
 		$Animations.flip_h = velocity.x < 0		
 	
+	# Move the player
 	position += velocity * delta
