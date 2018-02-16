6690c39b5c7db3e254cf346d81340ca47124466c
Aleksandr Hovhannisyan <aleksandrhovhannisyan@gmail.com>
2018-02-16 08:09:15 -0500

    New environment tiles and folders; modified scenes

--- entity_scenes/Player.gd
+++ entity_scenes/Player.gd
@@ -10,24 +10,33 @@ export (int) var stamina
 # The player's velocity
 var velocity = Vector2()
 
+# Defines the upward direction for the player as a unit vector
+const UP_DIRECTION = Vector2(0, -1)
+const GRAVITY = 10
+
 
 func _ready():	
 	pass
 
 
+# Processed every frame
 func _physics_process(delta):
 	
-	# Reset velocity to blank slate
-	velocity.y += 10	
+	# Update vertical velocity each frame with gravity
+	velocity.y += GRAVITY	
 	
 	# Player is moving to the right
 	if Input.is_action_pressed("ui_right"):
-		velocity.x += 5
+		velocity.x = speed
 		
 	# Player is moving to the left
 	elif Input.is_action_pressed("ui_left"):
-		velocity.x -= 5
-		
+		velocity.x = -speed
+	
+	elif Input.is_action_just_pressed("ui_up"):
+		if is_on_floor():
+			velocity.y = -1.5*speed
+	
 	elif Input.is_key_pressed(88):
 		$Animations.play("attacking")
 	
@@ -35,12 +44,12 @@ func _physics_process(delta):
 	else:
 		velocity.x = 0
 		$Animations.play("idle")
+	
 
 	# If player is moving horizontally
 	if velocity.x != 0:
-		velocity = velocity.normalized() * speed
 		$Animations.flip_h = velocity.x < 0	
 		$Animations.play("walking")
 	
 	# Move the player
-	move_and_slide(velocity)
+	velocity = move_and_slide(velocity, UP_DIRECTION)
