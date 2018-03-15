0d36294ee2b901a0472392e2a09be8f8b8ba55b5
Aleksandr Hovhannisyan <aleksandrhovhannisyan@gmail.com>
2018-03-15 07:27:12 -0400

    Major entity changes. Mob hitboxes now work perfectly. Flipping horizontally works. Consistent inheritance of hitboxes unless overriden.

--- entity_scenes/Mob.gd
+++ entity_scenes/Mob.gd
@@ -10,10 +10,8 @@ func _ready():
 func _physics_process(delta):
 	if $Animations.flip_h:
 		velocity.x = -speed
-		$Animations.position.x = -20
 	else:
 		velocity.x = speed
-		$Animations.position.x = -20
 	move(velocity)
 	pass
 
@@ -29,3 +27,5 @@ func _on_Area2D_body_entered(body):
 func _on_Area2D_body_exited( body ):
 	if body.collision_layer == 4:
 		update_state("walking")
+	elif body.collision_layer == 1:
+		velocity.y = 1.3*speed
