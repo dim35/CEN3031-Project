ddd42b373edb491a38fcccc65fc2fa057a1b799f
Jonathan <jlc3939@gmail.com>
2018-04-15 18:51:25 -0400

    Resolving conflicts

--- environment_scenes/World.gd
+++ environment_scenes/World.gd
@@ -1,5 +1,5 @@
 extends "res://Base.gd"
-export (PackedScene) var next_scene
+
 
 
 onready var entities = get_node("/root/World/entities")
@@ -27,9 +27,14 @@ func _ready():
 	global_player.connect("player_disconnect", self, "player_disconnect")
 	# tell server i'm ready to recieve player data
 	rpc_id(1, "feed_me_player_info", get_tree().get_network_unique_id())
+	rpc_id(1, "mark_player_as_spawned", get_tree().get_network_unique_id())
 	# set items to default amount (0)
-	for i in range(5):
+	for i in range(2):
 		inventory[i] = 0
+		
+	$PlayerHUD/Inventory/HealthPotion.set_inventory_item_count(0, 0)
+	$PlayerHUD/Inventory/StaminaPotion.set_inventory_item_count(1, 0)
+
 
 remote func spawn(who, id, it_id = 0, b = 0):
 	print("spawn! " + who + " " + str(id))
@@ -67,6 +72,7 @@ remote func spawn(who, id, it_id = 0, b = 0):
 	get_node("PlayerHUD/Stats/Health").set_parent_entity(local_player_instance)
 	get_node("PlayerHUD/Stats/Mana").set_parent_entity(local_player_instance)
 	get_node("PlayerHUD/Stats/Stamina").set_parent_entity(local_player_instance)
+	get_node("PlayerHUD/Inventory").set_parent_entity(local_player_instance)
 	
 
 func player_disconnect(id):
@@ -79,34 +85,26 @@ func player_disconnect(id):
 func _physics_process(delta):
 	if local_player_instance != null:
 		local_player_instance.move()
-	update_HUD_bars()
+	
+	# Use consumables and update HUD item tracker
+	if Input.is_action_just_released("use_item_0") && (inventory[0] > 0):
+		print("Drinking a health potion!")
+		update_inventory(0)
+		$PlayerHUD/Inventory/HealthPotion.set_inventory_item_count(0, inventory[0])
+		local_player_instance.use_item(0)
+		
+	elif Input.is_action_just_released("use_item_1") && (inventory[1] > 0):
+		print("Drinking a stamina potion!")
+		update_inventory(1)
+		$PlayerHUD/Inventory/StaminaPotion.set_inventory_item_count(1, inventory[1])
+		local_player_instance.use_item(1)	
 
-# Updates all player HUD bar maxima, dimensions, and current values
-func update_HUD_bars():
-	if local_player_instance == null:
-		return
-	var healthBar = $PlayerHUD/Stats/Health
-	var manaBar = $PlayerHUD/Stats/Mana
-	var staminaBar = $PlayerHUD/Stats/Stamina
-	
-	healthBar.set_max_value(local_player_instance.MAX_HEALTH)
-	healthBar.set_dimensions(local_player_instance.MAX_HEALTH)
-	healthBar.update(local_player_instance.health)
-	
-	manaBar.set_max_value(local_player_instance.MAX_MANA)
-	manaBar.set_dimensions(local_player_instance.MAX_MANA)
-	manaBar.update(local_player_instance.mana)
-	
-	staminaBar.set_max_value(local_player_instance.MAX_STAMINA)
-	staminaBar.set_dimensions(local_player_instance.MAX_STAMINA)
-	staminaBar.update(local_player_instance.stamina)		
 
-func levelOneComplete():
-	get_tree().change_scene_to(next_scene)
 
 # Triggered upon body entering the area. Used mainly for player entry. Triggers level end.
 func _on_GateArea_body_entered(body):
 	if body.collision_layer == PLAYER_COLLISION_LAYER:
+		$Gate.set_texture(load("res://assets/animation_sprites/environment/closed_gate.png"))
 		# $PlayerSpawner/Container.get_child(0).visible = false
 		$LevelEndTimer.start()
 
@@ -114,22 +112,25 @@ func _on_GateArea_body_entered(body):
 
 # Changes to splash screen
 func level_complete():
-	var my_scene = load("res://environment_scenes/World2.tscn")
-	get_tree().change_scene_to(next_scene)
+	var my_scene = load("res://screens/splash_screen/splash_screen.tscn")
+	get_tree().change_scene_to(my_scene)
 
 # When the timer reaches 0, trigger the end of the level
 func _on_LevelEndTimer_timeout():
 	level_complete()
 
 func item_picked_up(id):
-	# add another instance of item
-	inventory[id] = inventory[id] + 1
-	
-	print ("Inventory: " + str(inventory))
-	
-	# call some GUI update
-	
-	
+	# just to notify when player picked up item, perhaps for gui
+	if id == 0:
+		print("Picked up health potion")
+		$PlayerHUD/Inventory/HealthPotion.set_inventory_item_count(id, inventory[id])
+	elif id == 1:
+		print("Picked up stamina potion")
+		$PlayerHUD/Inventory/StaminaPotion.set_inventory_item_count(id, inventory[id])
+	else:
+		
+		print("Picked up some item, ", id)
+
 
 func _server_disconnected():
 	var my_scene = load("res://screens/login_screen/login_screen.tscn")
@@ -153,3 +154,14 @@ func menu():
 func quit_game():
 	print ("The game is quit")
 	get_tree().quit()
+	
+	
+remote func set_inventory(it):
+	print ("Got inventory" + str(it))
+	inventory = it
+	
+# originally a remote func
+func update_inventory(id):
+	inventory[id] -= 1
+	rpc_id(1, "update_inventory_from_client", get_tree().get_network_unique_id(), inventory)
+	
