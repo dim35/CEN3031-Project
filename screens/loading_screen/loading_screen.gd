3840c1598b658bd6bab07b5815e714ddd39203b3
RustyBamboo <dvolya@gmail.com>
2018-04-14 22:01:34 -0400

    Add loading screen and mark as ready

---
+++ screens/loading_screen/loading_screen.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+export (PackedScene) var next_scene
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+func _init():
+	global_player.connect("finished_loading", self, "finished_loading")
+
+func finished_loading():
+	get_tree().change_scene_to(next_scene)
+	queue_free()
+	
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
